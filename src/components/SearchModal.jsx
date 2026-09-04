51bf84304b585681130973bacaadaf122d80b1ff
muhammadhassan4129 <hassank7766khan@gmail.com>
2026-09-04 10:45:15 +0500

    update search modal

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const searchableData = [
   // Homepage Sections (Smooth Scroll targets)
